f95f3822f763d70f53242fbc18afcd89f989c666
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-12-05 13:41:33 +0100

    alter pipeline to fix bug with static files in staging

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 import million from 'million/compiler';
- 
-/** @type {import('next').NextConfig} */
+
+const isStaging = process.env.BUILD_ENV === 'staging';
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isStaging ? '/staging' : '', // Set the base path to '/staging' only for staging builds
 };
- 
+
 const millionConfig = {
-  auto: {rsc: true},
-}
- 
+  auto: { rsc: true },
+};
+
 export default million.next(nextConfig, millionConfig);
