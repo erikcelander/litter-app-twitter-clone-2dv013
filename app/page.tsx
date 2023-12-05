6ea7ee51ce7cae33592f47a1984a8e3a1dc93f94
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-12-05 12:52:05 +0100

    add staging in pipeline

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+import { createClient } from '@/lib/supabase/server'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
 
 export default async function Index() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
 
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) {
+    redirect('/login')
+  }
+  
 
   return (
     <div className=''>
-      test
+     {user.email}
     </div>
   )
 }
