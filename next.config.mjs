455dbd7f11c49ea0af99e687069f4f687fd930ff
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-18 19:51:02 +0100

    really fix bug this time

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 import million from 'million/compiler';
 
-// const isStaging = process.env.BUILD_ENV === 'staging';
-
 const nextConfig = {
   reactStrictMode: true,
- // basePath: isStaging ? '/staging' : '', // Set the base path to '/staging' only for staging builds
 };
 
 const millionConfig = {
