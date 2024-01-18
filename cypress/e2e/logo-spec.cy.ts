a168a38f25389b0668b3cccf5f239c3f991df23a
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-18 20:02:05 +0100

    add basic testing with cypress

--- cypress/e2e/logo-spec.cy.ts
+++ cypress/e2e/logo-spec.cy.ts
@@ -1,6 +1,7 @@
 describe('litter logo text test', () => {
   it('passes', () => {
     cy.visit('https://qa-cscloud7-103.lnu.se')
+    
     cy.get('.flex > .text-center').contains('litter')
   })
 })
