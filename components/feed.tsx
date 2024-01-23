c4c8baef3b96cbc91b98e7ee96ef5b4446360703
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-24 00:48:07 +0100

    test

--- components/feed.tsx
+++ components/feed.tsx
@@ -5,39 +5,45 @@ import { Lit } from '@/components/lit';
 import { useRouter } from 'next/navigation';
 import { createClient } from '@/lib/supabase/client';
 
+import { createBrowserClient } from '@supabase/ssr'
+
 
 export default function Feed({ lits, userId }: { lits: any[], userId?: string }) {
   // const supabase = createClient();
-  // const router = useRouter();
+  const router = useRouter();
 
+  const supabase =  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
 
-  // useEffect(() => {
-  //   const channel = supabase
-  //   .channel("realtime feed")
-  //   .on(
-  //     "postgres_changes",
-  //     userId
-  //       ? {
-  //           event: "*",
-  //           schema: "public",
-  //           table: "lits", 
-  //           filter: `user_id.eq.${userId}`
-  //         }
-  //       : {
-  //           event: "*",
-  //           schema: "public",
-  //           table: "lits", 
-  //         },
-  //     () => {
-  //       router.refresh();
-  //     }
-  //   )
-  //   .subscribe();
+  useEffect(() => {
+    const channel = supabase
+    .channel("realtime feed prod")
+    .on(
+      "postgres_changes",
+      userId
+        ? {
+            event: "*",
+            schema: "public",
+            table: "lits", 
+          //  filter: `user_id.eq.${userId}`
+          }
+        : {
+            event: "*",
+            schema: "public",
+            table: "lits", 
+          },
+      () => {
+        router.refresh();
+      }
+    )
+    .subscribe();
 
-  //   return () => {
-  //     supabase.removeChannel(channel);
-  //   };
-  // }, [supabase, router]);
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, router]);
 
   return (
     <div>
