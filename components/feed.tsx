7c68f37a5886af90500caea7940f0dab80d7a8c7
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-24 00:35:16 +0100

    test fix bug with supabase

--- components/feed.tsx
+++ components/feed.tsx
@@ -7,37 +7,37 @@ import { createClient } from '@/lib/supabase/client';
 
 
 export default function Feed({ lits, userId }: { lits: any[], userId?: string }) {
-  const supabase = createClient();
-  const router = useRouter();
+  // const supabase = createClient();
+  // const router = useRouter();
 
 
-  useEffect(() => {
-    const channel = supabase
-    .channel("realtime feed")
-    .on(
-      "postgres_changes",
-      userId
-        ? {
-            event: "*",
-            schema: "public",
-            table: "lits", 
-            filter: `user_id.eq.${userId}`
-          }
-        : {
-            event: "*",
-            schema: "public",
-            table: "lits", 
-          },
-      () => {
-        router.refresh();
-      }
-    )
-    .subscribe();
+  // useEffect(() => {
+  //   const channel = supabase
+  //   .channel("realtime feed")
+  //   .on(
+  //     "postgres_changes",
+  //     userId
+  //       ? {
+  //           event: "*",
+  //           schema: "public",
+  //           table: "lits", 
+  //           filter: `user_id.eq.${userId}`
+  //         }
+  //       : {
+  //           event: "*",
+  //           schema: "public",
+  //           table: "lits", 
+  //         },
+  //     () => {
+  //       router.refresh();
+  //     }
+  //   )
+  //   .subscribe();
 
-    return () => {
-      supabase.removeChannel(channel);
-    };
-  }, [supabase, router]);
+  //   return () => {
+  //     supabase.removeChannel(channel);
+  //   };
+  // }, [supabase, router]);
 
   return (
     <div>
