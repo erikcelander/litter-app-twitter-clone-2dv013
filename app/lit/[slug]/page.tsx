61d05aebe1e87ad380cdb98173690d013c1f76d8
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-24 00:11:57 +0100

    try to fix staging bug

--- app/lit/[slug]/page.tsx
+++ app/lit/[slug]/page.tsx
@@ -13,14 +13,26 @@ type LitProps = {
 async function getLit(slug: string) {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_LITTER_URL}/api/lits?lit=${slug}`)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     const data = await response.json()
-    return data as LitProps
+    if (Array.isArray(data)) {
+      if (data.length === 1) {
+        return data[0] as LitProps;
+      } else if (data.length === 0) {
+        console.error('No lits found with the provided slug');
+      } else {
+        console.error('Multiple lits found with the provided slug');
+      }
+    } else {
+      return data as LitProps;
+    }
   } catch (error: any) {
     console.error(error)
-  }
  
+  }
 }
-
 export default async function Page({ params }: { params: { slug: string } }) {
   const lit = await getLit(params.slug)
 
