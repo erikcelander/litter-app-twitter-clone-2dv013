a0947a948a9a204dec000e6c1d7f6914199f84fb
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-25 00:43:55 +0100

    comment out some code

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,28 @@ import { createSupabaseServer } from '@/lib/supabase/server'
 
 
 export default async function Index() {
-  const supabase = createSupabaseServer()
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
+  // const supabase = createSupabaseServer()
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession()
 
-  let lits
-  try {
-    const { data, error } = await supabase
-      .from('lits')
-      .select('*')
-      .order('created_at', { ascending: false })
+  // let lits
+  // try {
+  //   const { data, error } = await supabase
+  //     .from('lits')
+  //     .select('*')
+  //     .order('created_at', { ascending: false })
 
-    if (error) throw error
-    lits = data
-  } catch (error) {
-    console.error('Error fetching lits:', error)
-  }
+  //   if (error) throw error
+  //   lits = data
+  // } catch (error) {
+  //   console.error('Error fetching lits:', error)
+  // }
 
   return (
     <div className=''>
-      {session && <SubmitLit />}
+      hej
+      {/* {session && <SubmitLit />} */}
       {/*lits && <Feed lits={lits} />*/}
     </div>
   )
