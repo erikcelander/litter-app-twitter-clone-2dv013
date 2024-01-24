9a6d000269b9946b7182b0d9fd23151dbbf05b6e
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-24 21:47:02 +0100

    add debug statement

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -6,6 +6,7 @@ export const createSupabaseServer = () => {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL! as string
   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! as string
 
+  console.log("supabase url env: " + url)
   return createServerClient(url, key, {
     cookies: {
       get(name: string) {
