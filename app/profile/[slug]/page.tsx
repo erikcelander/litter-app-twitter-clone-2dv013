568b1cac7c422938187cf5abcb0ad178b136bf5f
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-24 21:49:46 +0100

    comment out code

--- app/profile/[slug]/page.tsx
+++ app/profile/[slug]/page.tsx
@@ -1,6 +1,6 @@
-// import Profile from '@/components/profile'
-// import { cookies } from 'next/headers'
-// import { createSupabaseServer } from '@/lib/supabase/server'
+import Profile from '@/components/profile'
+import { cookies } from 'next/headers'
+import { createSupabaseServer } from '@/lib/supabase/server'
 
 // async function fetchUserProfile(slug: string) {
 //   try {
@@ -12,25 +12,25 @@
 //   }
 // }
 
-// export default async function Page({ params }: { params: { slug: string } }) {
-//   const supabase = createSupabaseServer()
+export default async function Page({ params }: { params: { slug: string } }) {
+  // const supabase = createSupabaseServer()
 
 
-//   const user = await fetchUserProfile(params.slug)
+  // const user = await fetchUserProfile(params.slug)
 
-//   let lits = []
-//   try {
-//     const { data, error } = await supabase
-//       .from('lits')
-//       .select('*')
-//       .eq('user_id', user.id)
-//       .order('created_at', { ascending: false })
+  // let lits = []
+  // try {
+  //   const { data, error } = await supabase
+  //     .from('lits')
+  //     .select('*')
+  //     .eq('user_id', user.id)
+  //     .order('created_at', { ascending: false })
 
-//     if (error) throw error
-//     lits = data
-//   } catch (error) {
-//     console.error('Error fetching lits:', error)
-//   }
-
-//   return <div>{user && <Profile user={user} lits={lits} />}</div>
-// }
+  //   if (error) throw error
+  //   lits = data
+  // } catch (error) {
+  //   console.error('Error fetching lits:', error)
+  // }
+  return <div>profile</div>
+  // return <div>{user && <Profile user={user} lits={lits} />}</div>
+}
