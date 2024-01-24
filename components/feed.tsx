bbc38de5b2abff72135e046d03b534721fc723c2
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-24 18:32:52 +0100

    change router.refresh to usestate for realtime feed

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,38 +1,28 @@
 'use client'
-
 import React, { useEffect, useState } from 'react';
 import { Lit } from '@/components/lit';
 import { useRouter } from 'next/navigation';
 import { createClient } from '@/lib/supabase/client';
 
-
-
 export default function Feed({ lits, userId }: { lits: any[], userId?: string }) {
+  const [updatedLits, setLits] = useState<any[]>(lits);
   const supabase = createClient();
   const router = useRouter();
 
   useEffect(() => {
     const channel = supabase
-    .channel("realtime feed prod")
-    .on(
-      "postgres_changes",
-      userId
-        ? {
-            event: "*",
-            schema: "public",
-            table: "lits", 
-            filter: `user_id.eq.${userId}`
-          }
-        : {
-            event: "*",
-            schema: "public",
-            table: "lits", 
-          },
-      () => {
-        router.refresh();
-      }
-    )
-    .subscribe();
+      .channel("realtime feed prod")
+      .on("postgres_changes", {
+          event: "*",
+          schema: "public",
+          table: "lits",
+        },
+        (payload) => {
+          console.log('New payload:', payload); // Debugging line
+          setLits(current => [payload.new, ...current]);
+        }
+      )
+      .subscribe();
 
     return () => {
       supabase.removeChannel(channel);
@@ -41,7 +31,7 @@ export default function Feed({ lits, userId }: { lits: any[], userId?: string })
 
   return (
     <div>
-      {lits.map((lit: any) => (
+      {updatedLits.map((lit: any) => (
         <Lit
           key={lit.id}
           username={lit.username}
