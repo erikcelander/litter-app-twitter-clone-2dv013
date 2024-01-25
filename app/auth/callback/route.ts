dd51fa016ccb8df6ae68955b1aee0224a867f52c
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-01-25 15:16:13 +0100

    add debugging logs

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,10 +3,13 @@ import { NextResponse } from 'next/server'
 import { type CookieOptions, createServerClient } from '@supabase/ssr'
 
 export async function GET(request: Request) {
+  console.log(1)
+  console.log(request)
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
-
+  console.log(code)
   if (code) {
+    console.log(2)
     const cookieStore = cookies()
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -25,15 +28,26 @@ export async function GET(request: Request) {
         },
       }
     )
+    console.log(3)
+    let response
+    try {
+      response = await supabase.auth.exchangeCodeForSession(code)
+      console.log(response)
+      const { error } = response
+      console.log(4)
 
-    const response = await supabase.auth.exchangeCodeForSession(code)
-    const { error } = response
-
-
-    if (!error) {
-      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_LITTER_URL}`)
+      if (error) console.log(error)
+      if (!error) {
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_LITTER_URL}`)
+      }
+    } catch (error) {
+      if (error) console.log(error)
+      if (response) console.log(response)
     }
+
+    console.log(5)
   }
+  console.log(6)
 
   // return the user to an error page with instructions
   return NextResponse.redirect(`${process.env.NEXT_PUBLIC_LITTER_URL}/auth/auth-code-error`)
---
+++ lib/supabase/code.ts
@@ -0,0 +1,17 @@
+import { createClient } from "@supabase/supabase-js";
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL! as string
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! as string
+
+export const supabase = createClient(
+  url,
+  key,
+  {
+    auth: {
+      flowType: "pkce",
+      autoRefreshToken: false,
+      detectSessionInUrl: false,
+      persistSession: true,
+    },
+  },
+);
