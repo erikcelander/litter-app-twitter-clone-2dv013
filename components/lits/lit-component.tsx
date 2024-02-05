52593780951d2d146da48f28138fc2ce3fe213bc
Amanda Möllestål <am224pg@student.lnu.se>
2024-02-05 14:04:42 +0100

    Style lit component

--- components/lits/lit-component.tsx
+++ components/lits/lit-component.tsx
@@ -3,29 +3,47 @@ import { Separator } from "@/components/ui/separator"
 import Link from 'next/link';
 import { Lit } from "@/lib/types";
 
-export const  LitComponent = ({ lit }: { lit: Lit }) => {
+export const LitComponent = ({ lit }: { lit: Lit }) => {
   return (
-    <div className="flex gap-4 p-4  text-white  max-w-md mx-auto">
-    <Link className='hover:cursor-pointer' href={`/profile/${lit?.username}`}>
-    <div className="flex flex-col items-center">
-      <Avatar className="h-10 w-10">
-        <AvatarImage alt={`@${lit?.username}`} src={lit?.avatar_url!} />
-        <AvatarFallback>{lit?.full_name ? lit?.full_name.charAt(0) : "U"}</AvatarFallback>
-      </Avatar>
-      <div className="grid gap-0.5 text-xs mt-2">
-        <div className="font-medium">{lit?.full_name || "Unknown User"}</div>
-        <div className="text-gray-500 dark:text-gray-400 text-center">@{lit?.username || "unknown"}</div>
+    <div className=" p-2 pt-4 pb-4 text-white  max-w-xl mx-auto">
+      <div className="flex flex-row min-w-xl" style={{ width: '470px' }}>
+        <Link className='hover:cursor-pointer' href={`/profile/${lit?.username}`}>
+
+          <Avatar className="h-12 w-12 mt-auto mb-auto">
+            <AvatarImage alt={`@${lit?.username}`} src={lit?.avatar_url!} />
+            <AvatarFallback>{lit?.full_name ? lit?.full_name.charAt(0) : "U"}</AvatarFallback>
+          </Avatar>
+        </Link>
+
+        <div className="flex flex-col justify-center">
+
+            <div className="ml-5  flex flex-row" style={{ width: '470px' }}>
+            <Link className='hover:cursor-pointer flex flex-row' href={`/profile/${lit?.username}`}>
+
+              <div className="hover:underline text-primary/100 text-lg">{lit?.full_name || "Unknown User"}</div>
+              <div className="hover:underline text-primary/40 text-base  mt-0.5 ml-2">@{lit?.username || "unknown"}</div>
+              </Link>
+
+            </div>
+
+          <Link className='hover:cursor-pointer text-gray-100' href={`/lit/${lit?.id}`}>
+
+            <div className="flex ml-5  pl-0">
+              <p className="text-sm">
+                {lit?.content || "No content available."}
+              </p>
+            </div>
+          </Link>
+
+        </div>
+
       </div>
-    </div>
-    </Link>
 
-    <div><Separator className='bg-primary' orientation="vertical" /></div>
 
-    <div className="flex w-56 p-6 pl-3">
-      <p className="text-sm">
-        {lit?.content || "No content available."}
-      </p>
+
+
+
+
     </div>
-  </div>
   )
 }
