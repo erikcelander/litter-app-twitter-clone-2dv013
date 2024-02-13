d2d17b2ddb86bb2accc8d1c4b3dbb7800782d96a
Erik Kroon Celander <ek223ur@student.lnu.se>
2024-02-13 10:39:45 +0100

    Fix test bug

--- cypress/e2e/lit-spec.cy.ts
+++ cypress/e2e/lit-spec.cy.ts
@@ -2,7 +2,9 @@ describe('lit spec', () => {
   it('passes', () => {
     cy.visit(Cypress.env('prod_url') + '/lit/27904c65-abf5-466d-974a-e371bd3a30bc')
 
-    cy.get('.min-w-xl > .flex-col').contains('cats')
+    cy.wait(1500)
+
+    cy.get('.pr-2 > .flex-col').contains('cats')
 
     cy.get('.ml-5.flex-row').contains('@ek223ur')
   })
